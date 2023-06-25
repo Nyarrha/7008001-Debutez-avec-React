4c177c8f2b50dd287632426cfac3c7deea0be122
Nyarrha <paradisquentin@yahoo.fr>
2023-06-25 16:06:18 +0200

    P2C5 exo

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,5 +1,32 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
+// import { useState } from 'react'
+
+// function howMuchWaterCare(value) {
+// 	switch(value){
+// 		case '1':
+// 			alert('Cette plante requiert peu d\'arrosage.')
+// 			break;
+// 		case '2':
+// 			alert('Cette plante requiert modérément d\'arrosage.')
+// 			break;
+// 		case '3':
+// 			alert('Cette plante requiert beaucoup d\'arrosage.')
+// 	}
+// }
+
+// function howMuchWaterCare(value) {
+// 	switch(value){
+// 		case '1':
+// 			alert('Cette plante requiert peu de lumière.')
+// 			break;
+// 		case '2':
+// 			alert('Cette plante requiert modérément de lumière.')
+// 			break;
+// 		case '3':
+// 			alert('Cette plante requiert beaucoup de lumière.')
+// 	}
+// }
 
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
@@ -14,7 +41,14 @@ function CareScale({ scaleValue, careType }) {
 		<div>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
-					<span key={rangeElem.toString()}>{scaleType}</span>
+					<span key={rangeElem.toString()} /*onClick={()=> {
+					// 	howMuchWaterCare(rangeElem)
+					// 	howMuchLightCare(rangeElem)
+					// 	}
+					// 	}*/
+					>
+							{scaleType}
+					</span>
 				) : null
 			)}
 		</div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import '../styles/Footer.css'
 
+
 function Footer() {
-	const [inputValue, setInputValue] = useState('')
+	const [inputValue, setInputValue] = useState('exemple@adresse.mail')
+	function isMailValid(value){
+		if (!value.includes('@')) {
+			alert('Adresse invalide. Veuillez utiliser un @ dans votre adresse.')
+			setInputValue(value)
+		}
+	}
 
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<div className='lmj-footer-elem'>Laissez-nous votre mail :
+				<form>
+					<textarea
+						value={inputValue}
+						onChange={(e)=> setInputValue(e.target.value)}
+						onBlur={(e) => isMailValid(e.target.value)}
+					/>
+					<button type='submit'>Envoyer</button>
+				</form>
+			</div>
 		</footer>
 	)
 }
